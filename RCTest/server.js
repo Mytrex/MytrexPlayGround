c19ffa57bf11f278478461743936acfa88e1a853
Zach Parduhn <zacharyparduhn@gmail.com>
2020-03-24 15:01:20 -0600

    Updated with .env, retry, refresh token ect

--- RCTest/server.js
+++ RCTest/server.js
@@ -2,13 +2,13 @@
 require('dotenv').config();
 const express = require('express'),
 bodyParser = require('body-parser'),
-session = require('express-session'),
+// session = require('express-session'), //Not sure if we can take advantage of this or not.
 path = require('path');
 
 
 //Local Requires;
 const authorize = require('./source/authorize.js'), 
-ringout = require('./source/ringout.js');
+ringOut = require('./source/ringout.js');
 
 
 var app = express();
@@ -19,135 +19,8 @@ var jsonParser = bodyParser.json({
 });
 
 app.use(jsonParser);
-app.use(session({ secret: 'somesecretstring', tokens: ''}));
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
 
-const RINGCENTRAL_CLIENT_ID= process.env.RINGCENTRAL_CLIENT_ID;
-const RINGCENTRAL_CLIENT_SECRET= process.env.RINGCENTRAL_CLIENT_SECRET;
-const RINGCENTRAL_SERVER_URL= process.env.RINGCENTRAL_SERVER_URL;
-const RINGCENTRAL_REDIRECT_URL= process.env.RINGCENTRAL_REDIRECT_URL;
-
-var RingCentral = require('@ringcentral/sdk').SDK;
-var rcsdk = new RingCentral({
-  server: RINGCENTRAL_SERVER_URL,
-  clientId: RINGCENTRAL_CLIENT_ID,
-  clientSecret: RINGCENTRAL_CLIENT_SECRET,
-  redirectUri: RINGCENTRAL_REDIRECT_URL
-});
-
-//client secret: "cGvMFZ0TSYaZB95-DXGLZgt0S_3i98T3aB25OGfBmqqQ"
-//client ID: "NpUS1WMFSx2uyLbzJNAp4A"
-//PhoneNumber: 14703940075
-app.get('/ringout', authorize.authorize, ringout.ringOut);
-app.get('/index', function (req, res) {
-  res.redirect("/")
-})
-app.get('/', function (req, res) {
-    var platform = rcsdk.platform()
-    if (req.session.tokens != undefined){
-        var tokensObj = req.session.tokens
-        platform.auth().setData(tokensObj);
-        platform.loggedIn().then(function(isLoggedIn) {
-          if (isLoggedIn) {
-            return res.render('test')
-          }
-          res.render('index', {
-              authorize_uri: platform.loginUrl({
-                brandId: ''
-              })
-          });
-        })
-        return;
-    }
-    res.render('index', {
-        authorize_uri: platform.loginUrl({
-          brandId: ''
-        })
-    });
-})
-
-app.get('/logout', function(req, res) {
-  if (req.session.tokens != undefined){
-      var tokensObj = req.session.tokens
-      var platform = rcsdk.platform()
-      platform.auth().setData(tokensObj)
-      platform.loggedIn().then(function(isLoggedIn) {
-        if (isLoggedIn) {
-          platform.logout()
-            .then(function(resp){
-                console.log("logged out")
-            })
-            .catch(function(e){
-                console.log(e)
-            });
-        }
-        req.session.tokens = null
-        res.redirect("/")
-      });
-      return
-  }
-  res.redirect("/")
-})
-
-app.get('/oauth2callback', function(req, res) {
-  if (req.query.code) {
-      var platform = rcsdk.platform()
-      platform.login({
-          code: req.query.code,
-      })
-      .then(function(response) {
-        return response.json()
-      })
-      .then(function (token) {
-          req.session.tokens = token
-          res.redirect("/test")
-      })
-      .catch(function (e) {
-          res.send('Login error ' + e)
-      });
-  }else {
-      res.send('No Auth code');
-  }
-});
-
-app.get('/test', function(req, res) {
-  if (req.session.tokens != undefined){
-      var tokensObj = req.session.tokens
-      var platform = rcsdk.platform()
-      platform.auth().setData(tokensObj)
-      platform.loggedIn().then(function(isLoggedIn) {
-        if (isLoggedIn) {
-          if (req.query.api == "extension"){
-            var endpoint = "/restapi/v1.0/account/~/extension"
-            return callGetEndpoint(platform, endpoint, res)
-          } else if (req.query.api == "extension-call-log"){
-            var endpoint = "/restapi/v1.0/account/~/extension/~/call-log"
-            return callGetEndpoint(platform, endpoint, res)
-          } if (req.query.api == "account-call-log"){
-            var endpoint = "/restapi/v1.0/account/~/call-log"
-            return callGetEndpoint(platform, endpoint, res)
-          }
-        }
-        res.redirect("/")
-      })
-      return;
-  }
-  res.redirect("/")
-});
-
-function callGetEndpoint(platform, endpoint, res){
-    platform.get(endpoint)
-    .then(function(resp){
-      return resp.json()
-    })
-    .then(function(json) {
-      res.send(JSON.stringify(json))
-    })
-    .catch(function(e){
-        res.send("Error")
-    })
-}
+app.get('/ringOut', authorize.authorize, ringOut.ringOut);
 
 var server = app.listen(5000, function () {
   console.log("listening on port 5000")
--- RCTest/source/ringout.js
+++ RCTest/source/ringout.js
@@ -2,33 +2,46 @@ const request = require('request');
 const urlHost = 'https://platform.devtest.ringcentral.com';
 module.exports = {
     ringOut: function(req, res, next){
-
+        let tryCount = 0;
+        let body = req.body;
+        let toPhone = body.toPhone;
+        let fromPhone = body.fromPhone;
+        let callerId = body.callerId;
         let headers = {
             "Content-Type": "application/json",
             "Accept": "application/json",
             "Authorization": "bearer "+req.access_token
         }
-        let body = {
-            "from": {"phoneNumber": "14703940075"},
-            "to": {"phoneNumber": "18016692880"},
-            "playPrompt": true,
+        let postBody = {
+            "from": {"phoneNumber": fromPhone},
+            "to": {"phoneNumber": toPhone},
+            "playPrompt": (body.playPrompt)? body.playPrompt : true,
             "country": { "id" : "1" },
-            "callerId": {"phoneNumber": "14703940075"}
+            "callerId": {"phoneNumber": callerId}
         }
         let uri = urlHost + '/restapi/v1.0/account/~/extension/~/ring-out';
         return request.post({
                 url: uri,
                 headers: headers,
-                body: body,
+                body: postBody,
                 json: true
             },
             function (error, response, body) {
                 if (!error && response && response.statusCode === 200) {
                     res.status(200).json(body)
                 } else {
-                     res.status(400).json(body)
-                }
+                    if (tryCount < 2){
+                        tryCount ++;
+                        authorize.authorizeCB(req, function (err, res) {
+                            if(err){
 
+                            } else {
+
+                            }
+                        })
+                    }
+                    res.status(400).json(body)
+                }
             }
         )
     }
